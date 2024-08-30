97d5995ab1ac184f0be902da56af8ec49c61f927
volkov <x514@list.ru>
2024-08-30 13:49:40 +0300

    Add settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+# Cerebro settings
+CEREBRO_DB_HOST = "192.168.1.1"
+CEREBRO_DB_PORT = "5432"
+CARGADOR_HOST = "192.168.1.2"
+CARGADOR_HTTP_PORT = "45430"
+CARGADOR_XMLRPC_PORT = "45430"
+MIRADA_PATH = "//ss/front/cerebro/mirada.exe"
+
+# Basic Cerebro User
+CEREBRO_USER_LOGIN = "rnd"
+CEREBRO_USER_PASSWORD = "1"
+
