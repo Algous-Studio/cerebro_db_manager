de3de59bbd12968378ec9686f4b9bc8a836ed27a
Artem <110411129+NailsRumford@users.noreply.github.com>
2024-09-18 12:05:11 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import sys
 from setuptools import setup, find_packages
 
-# Открытие README.md для описания пакета
 f = open('README.md')
 readme = f.read().strip()
+
 
 setup(
     name='cerebro_db_manager',
     version='0.1.0',
     description='Cerebro Production Tracking Python API Wrapper',
     long_description=readme,
-    long_description_content_type='text/markdown',
     author='Algous',
     author_email='info@algousstudio.ru',
     url='https://github.com/Algous-Studio/cerebro_db_manager',
@@ -19,7 +18,6 @@ setup(
     install_requires=[
         'psycopg2-binary',
         'Pillow>=8.0.0',
-        'git+https://github.com/Algous-Studio/cerebro_db_manager.git@td-671-dobavit-cerebro_db_manager-v-addon-cerebro_addon-chtobi-im#egg=cerebro_db_manager',
     ],
     zip_safe=False,
     python_requires=">=3.7.0",
