703e1bedba099b141fe6b9a251be13b2601f562b
Artem <110411129+NailsRumford@users.noreply.github.com>
2024-11-01 14:52:20 +0300

    добавил функцию для создания тумбнейлов

--- cerebro_db_manager/attachment.py
+++ cerebro_db_manager/attachment.py
@@ -1,6 +1,9 @@
 import os
 import logging
+import subprocess
+import fnmatch
 from PIL import Image
+import settings
 
 logger = logging.getLogger(__name__)
 
@@ -41,3 +44,16 @@ class Attachment:
                 valid_thumbnails.append(thumbnail_path)
 
             self.thumbnails = valid_thumbnails[:3]
+
+def generate_thumbnails(thumbnail_dir, file_path):   
+    res_code = subprocess.call([settings.MIRADA_PATH, '--mode', 'thumb', file_path, '--temp', thumbnail_dir])
+    if res_code != 0:
+        raise Exception("Mirada returned a non-zero exit status.\n" + settings.MIRADA_PATH)
+    
+    thumbnails = []
+    for f in os.listdir(thumbnail_dir):
+        if fnmatch.fnmatch(f, os.path.basename(file_path) + '.thumb--3-*.jpg'):
+            thumbnails.append(os.path.join(thumbnail_dir, f))
+    
+    thumbnails.sort()
+    return thumbnails
